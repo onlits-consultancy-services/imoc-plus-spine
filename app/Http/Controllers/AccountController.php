0ebeaf43892432a736754f99e50520dcb0fd2184
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-10-19 14:09:54 +0530

    Work on new account-group module Fixes #5

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,40 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\Account;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreAccountRequest;
+use App\Http\Requests\UpdateAccountRequest;
 
 class AccountController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return Account::all();
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
     }
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreAccountRequest $request)
     {
-        $account = new Account();
-        $account->name = $request->name;
-        $account->group = $request->group;
-        $account->opening_balance = $request->opening_balance;
-        $account->save();
+        //
     }
 
     /**
      * Display the specified resource.
-     *
-     * @param  \App\Models\Account  $account
-     * @return \Illuminate\Http\Response
      */
     public function show(Account $account)
     {
@@ -44,22 +41,23 @@ class AccountController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Account  $account
-     * @return \Illuminate\Http\Response
+     * Show the form for editing the specified resource.
      */
-    public function update(Request $request, Account $account)
+    public function edit(Account $account)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAccountRequest $request, Account $account)
     {
         //
     }
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Account  $account
-     * @return \Illuminate\Http\Response
      */
     public function destroy(Account $account)
     {
